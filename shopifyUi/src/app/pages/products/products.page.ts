62d476142794b14422400d7c16c9921be7784ea3
Charlton Adams <charltonadams38@gmail.com>
2024-05-30 12:43:46 +0200

    add products slide on products page

--- shopifyUi/src/app/pages/products/products.page.ts
+++ shopifyUi/src/app/pages/products/products.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { StatusBar, Style } from '@capacitor/status-bar';
+import { Product } from 'src/app/interfaces/interfaces';
 
 @Component({
   selector: 'app-products',
@@ -8,24 +11,46 @@ import { Component, OnInit } from '@angular/core';
 export class ProductsPage implements OnInit {
 
   items = [
-    { icon: 'pricetag', name:'All products' },
-    { icon: 'pricetags', name:'Collections' },
-    { icon: 'storefront', name:'Inventory' },
-    { icon: 'enter', name:'Transfers' },
-    { icon: 'gift', name:'Gift cards' },
+    { icon: 'pricetag', name: 'All products' },
+    { icon: 'pricetags', name: 'Collections' },
+    { icon: 'storefront', name: 'Inventory' },
+    { icon: 'enter', name: 'Transfers' },
+    { icon: 'gift', name: 'Gift cards' },
   ];
 
-  constructor() { }
+  @ViewChild('content') content!: HTMLIonContentElement;
+  products: Product[] = [];
+
+  constructor(
+    private http: HttpClient,
+  ) { }
 
   ngOnInit() {
+    this.loadProducts();
+
+    // IOS only
     window.addEventListener('statusTap', () => {
-      console.log('status tap');
+      this.content.scrollToTop(1000);
     });
+
+    // Display content under transparent status bar (Android only)
+    StatusBar.setOverlaysWebView({ overlay: true });
   }
 
   doRefresh(event: any) {
     setTimeout(() => {
       event.target.complete();
     }, 2000);
+  }
+
+  loadProducts() {
+    this.http.get<Product[]>('https://fakestoreapi.com/products')
+      .subscribe(resp => {
+        console.log(
+          '🚀 ~ file: products.page.ts ~ line 50 ~ ProductsPage ~ .subscribe ~ resp',
+          resp
+        );
+        this.products = resp;
+      });
   }
 }
